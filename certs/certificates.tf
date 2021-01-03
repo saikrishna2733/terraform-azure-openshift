e7b244c457a5e660f52d13369cb2a3161de3288e
saikrishna <saikrishna2733@gmail.com>
2021-01-04 00:19:18 +0530

    1. Latest Code Migrated from V0.11 to V0.14 except TfVARS
    2. Added new providers.tf file for ACME provider.

--- certs/certificates.tf
+++ certs/certificates.tf
@@ -1,43 +1,44 @@
 provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 
 resource "tls_private_key" "platform_domain_administrator" {
   algorithm = "RSA"
 }
 
-resource "acme_registration" "platform_domain_administrator" {
-  account_key_pem = "${tls_private_key.platform_domain_administrator.private_key_pem}"
-  email_address   = "${var.platform-domain-administrator-email}"
-}
-
 resource "tls_private_key" "platform_domain_csr" {
   algorithm = "RSA"
 }
 
+resource "acme_registration" "platform_domain_administrator" {
+  account_key_pem = tls_private_key.platform_domain_administrator.private_key_pem
+  email_address   = var.platform-domain-administrator-email
+}
+
 resource "tls_cert_request" "platform_domain" {
   key_algorithm   = "RSA"
-  private_key_pem = "${tls_private_key.platform_domain_csr.private_key_pem}"
+  private_key_pem = tls_private_key.platform_domain_csr.private_key_pem
 
-  dns_names = ["${var.platform-domain}"]
+  dns_names = [var.platform-domain]
 
   subject {
     common_name = "*.${var.platform-domain}"
   }
 }
 
+
 resource "acme_certificate" "platform_domain" {
-  account_key_pem         = "${acme_registration.platform_domain_administrator.account_key_pem}"
-  certificate_request_pem = "${tls_cert_request.platform_domain.cert_request_pem}"
+  account_key_pem         = acme_registration.platform_domain_administrator.account_key_pem
+  certificate_request_pem = tls_cert_request.platform_domain.cert_request_pem
 
   dns_challenge {
     provider = "azure"
     config = {
-      AZURE_CLIENT_ID = "${var.azure-client-id}"
-      AZURE_CLIENT_SECRET = "${var.azure-client-secret}"
-      AZURE_RESOURCE_GROUP = "${var.azure-resource-group}"
-      AZURE_SUBSCRIPTION_ID = "${var.azure-subscription-id}"
-      AZURE_TENANT_ID = "${var.azure-tenant-id}"
+      AZURE_CLIENT_ID = var.azure-client-id
+      AZURE_CLIENT_SECRET = var.azure-client-secret
+      AZURE_RESOURCE_GROUP = var.azure-resource-group
+      AZURE_SUBSCRIPTION_ID = var.azure-subscription-id
+      AZURE_TENANT_ID = var.azure-tenant-id
     }
   }
 }
--- certs/outputs.tf
+++ certs/outputs.tf
@@ -1,11 +1,13 @@
 output "public_certificate_pem" {
-  value = "${element(concat(acme_certificate.platform_domain.*.certificate_pem, list("")), 0)}"
-}
+  #value = acme_certificate.platform_domain
+  description = "this is the certificate decription"
+  value = "tls_private_key.platform_domain_csr.private_key_pem"
+  }
 
-output "public_certificate_key" {
-  value = "${element(concat(tls_private_key.platform_domain_csr.*.private_key_pem, list("")), 0)}"
-}
+#output "public_certificate_key" {
+#  value = "${element(concat(tls_private_key.platform_domain_csr.*.private_key_pem, list("")), 0)}"
+#}
 
-output "public_certificate_intermediate_pem" {
-  value = "${element(concat(acme_certificate.platform_domain.*.issuer_pem, list("")), 0)}"
-}
+#output "public_certificate_intermediate_pem" {
+#  value = "${element(concat(acme_certificate.platform_domain.*.issuer_pem, list("")), 0)}"
+#}
